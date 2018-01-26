4d94927dd75559ce03d229387eaf07e6554c5e29
Mikhail Tatsky <mikhail.tatsky@gmail.com>
2018-01-26 20:48:54 +0300

    v1.0.1

--- dist/fancygrid-react.js
+++ dist/fancygrid-react.js
@@ -1,7 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
-
-import Fancy from 'fancygrid';
+import Fancy from "fancygrid";
 
 class Grid extends React.Component {
   render() {
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,4 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
+import React from 'react';
 import Fancy from 'fancygrid';
 
 class Grid extends React.Component {
